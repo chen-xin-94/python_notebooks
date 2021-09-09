97093cf93c9f580121405f81dd36bb20c739f2a0
chen-xin-94 <xinchenqd@gmail.com>
2021-09-09 09:56:17 +0200

    autoclick

---
+++ autoclick.py
@@ -0,0 +1,8 @@
+import time
+import pyautogui
+
+# move to the center of the monitor and click once every minute
+while True:
+    pyautogui.click(pyautogui.size().width/2,pyautogui.size().height/2)
+    time.sleep(60)
+
